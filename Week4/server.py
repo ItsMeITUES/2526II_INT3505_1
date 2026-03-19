0a1cb9403dfeab398de3b613bec8ba2d27848438
ItsMeITUES <dunggamer05@gmail.com>
2026-03-19 21:52:37 +0700

    Added Authorization with login / register

--- Week4/server.py
+++ Week4/server.py
@@ -23,16 +23,24 @@ APIVERSION = 'v3'
 
 BOOKAPI_URL = f'/api/{APIVERSION}/books'
 
+AUTHAPI_URL = f'/api/{APIVERSION}/auth'
+
 allowCache = True
 cacheVisibility = 'public' # public, private, no-cache
 cacheDuration = 60 # seconds
 
-API_KEY = os.getenv('API_KEY', 'default_api_key')
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'default_db_password')
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+tokenExpirationTime =  600 # seconds
 
 # Database
 booksDatabase = 'data/books.json'
 usersDatabase = 'data/users.json'
+
+######################
+# Database Functions #
+######################
 
 # Load books from the JSON file
 def load_books():
@@ -66,9 +74,103 @@ def save_users(users):
     with open(usersDatabase, 'w', encoding='utf-8') as f:
         json.dump(users, f, indent=4)
 
-#################
-# API Endpoints #
-#################
+############################
+# Authentication Endpoints #
+############################
+
+@app.route(f'{AUTHAPI_URL}/login', methods = ['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username:
+        return jsonify({"error":"Username is required."}), 400
+    if not password:
+        return jsonify({"error":"Password is required."}), 400
+
+    users = load_users()
+
+    user_password_hash = None
+
+    for user in users:
+        if user['username'] == username:
+            user_password_hash = user['password'].encode('utf-8')
+            break
+    
+    if not user_password_hash:
+        return jsonify({"error":"Unregistered user."}), 401
+
+
+    if bcrypt.checkpw(password.encode('utf-8'), user_password_hash):
+        now = datetime.now(timezone.utc)
+        
+        payload = {
+            'user_id': username,
+            'role': 'admin',
+            'exp': now + timedelta(seconds=tokenExpirationTime),
+            'iat': now
+        }
+
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')  
+
+        return jsonify({"accessToken": token})
+    
+    return jsonify({"error": "Wrong password."}), 401
+
+@app.route(f'{AUTHAPI_URL}/register', methods = ['POST'])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({"error":"Username and password are required."}), 400
+
+    users = load_users()
+    for user in users:
+        if user['username'] == username:
+            return jsonify({"error":"Username taken."}), 400
+    
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    new_user = {
+        'username': username,
+        'password': hashed_password.decode('utf-8')
+    }
+
+    users.append(new_user)
+    save_users(users)
+
+    return jsonify({"message": "User registered successfully."}), 201
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'Authorization' in request.headers:
+            auth_header = request.headers['Authorization']
+            token = auth_header.split(" ")[1] if " " in auth_header else None
+
+        if not token:
+            return jsonify({"error":"No token received."}), 401
+
+        try:
+            data = jwt.decode(token, SECRET_KEY, algorithms='HS256')
+            current_user = data['user_id']
+        except jwt.ExpiredSignatureError:
+            return jsonify({"error": "Token expired."}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({"error": "Token invalid."}), 401
+        
+        return f(current_user, *args, **kwargs)
+    
+    return decorated
+
+######################
+# Book API Endpoints #
+######################
 
 # Get all books
 @app.route(BOOKAPI_URL, methods=['GET'])
@@ -84,7 +186,8 @@ def get_books_with_cache():
 
 # Add a new book
 @app.route(BOOKAPI_URL, methods=['POST'])
-def add_book():
+@token_required
+def add_book(current_user):
     new_book = request.get_json()
 
     #1. Validate the input
@@ -96,8 +199,13 @@ def add_book():
 
         return jsonify({"error": "Invalid input"}), 400
 
-    #2. Add the new book to the list
     books = load_books()
+
+    for b in books:
+        if b['title'] == new_book['title'] and b['author'] == new_book['author']:
+            return jsonify({"error": "Book already exists"}), 400
+
+    #2. Add the new book to the list
 
     # New ID = max existing ID + 1, or 1 if the list is empty
     newId = max([b['id'] for b in books]) + 1 if books else 1
@@ -120,14 +228,18 @@ def get_book(book_id):
     for b in books:
         if b['id'] == book_id:
             book = b
-            return jsonify(book), 200
+            response = make_response(jsonify(book))
+            if allowCache:  
+                response.headers['Cache-Control'] = f'{cacheVisibility}, max-age={cacheDuration}'
+            return response, 200
     
     # If the book is not found, return a 404 error
     return jsonify({"error": "Book not found"}), 404
 
 # Delete a book by ID
 @app.route(f'{BOOKAPI_URL}/<int:book_id>', methods=['DELETE'])
-def delete_book(book_id):
+@token_required
+def delete_book(current_user, book_id):
 
     books = load_books()
 
@@ -136,14 +248,15 @@ def delete_book(book_id):
         if b['id'] == book_id:
             books.remove(b)
             save_books(books)
-            return jsonify({"message": "Book deleted"}), 200
+            return jsonify({"message": f"Book with ID {book_id} deleted successfully."}), 200
     
     # If the book is not found, return a 404 error
     return jsonify({"error": "Book not found"}), 404
 
 # Update a book by ID
 @app.route(f'{BOOKAPI_URL}/<int:book_id>', methods=['PUT'])
-def update_book(book_id):
+@token_required
+def update_book(current_user, book_id):
     updated_book = request.get_json()
 
     #1. Validate the input
@@ -174,7 +287,8 @@ def update_book(book_id):
 
 # Partially update a book by ID
 @app.route(f'{BOOKAPI_URL}/<int:book_id>', methods=['PATCH'])
-def partially_update_book(book_id):
+@token_required
+def partially_update_book(current_user, book_id):
     updated_fields = request.get_json()
 
     #1. Validate the input
@@ -198,86 +312,7 @@ def partially_update_book(book_id):
     #3. If the book is not found, return a 404 error
     return jsonify({"error": "Book not found"}), 404
 
-# Authentication Endpoints
-@app.route('/api/login', methods = ['POST'])
-def login():
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
 
-    users = load_users()
-
-    for user in users:
-        if user['username'] == username:
-            user_password_hash = user['password'].encode('utf-8')
-            break
-    
-    if not user_password_hash:
-        return jsonify({"error":"Unregistered user."}), 401
-
-
-    if bcrypt.checkpw(password.encode('utf-8'), user_password_hash):
-        now = datetime.now(timezone.utc)
-        
-        payload = {
-            'user_id': username,
-            'role': 'admin',
-            'exp': now + timedelta(hours=1),
-            'iat': now
-        }
-
-        token = jwt.encode(payload, API_KEY, algorithm='HS256')
-
-        return jsonify({"accessToken": token})
-    
-    return jsonify({"error": "Wrong password."}), 401
-
-@app.route('/api/register', methods = ['POST'])
-def register():
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
-
-    users = load_users()
-
-    if username in users:
-        return jsonify({"error":"Username taken."}), 400
-    
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-    new_user = {
-        'username': username,
-        'password': hashed_password.decode('utf-8')
-    }
-
-    users.append(new_user)
-    save_users(users)
-
-    return jsonify({"message": "User registered successfully."}), 201
-
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-
-        if 'Authorization' in request.headers:
-            auth_header = request.headers['Authorization']
-            token = auth_header.split(" ")[1] if " " in auth_header else None
-
-        if not token:
-            return jsonify({"error":"No token received."}), 401
-
-        try:
-            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms='HS256')
-            current_user = data['user_id']
-        except jwt.ExpiredSignatureError:
-            return jsonify({"error": "Token expired."}), 401
-        except jwt.InvalidTokenError:
-            return jsonify({"error": "Token invalid."}), 401
-        
-        return f(current_user, *args, **kwargs)
-    
-    return decorated
 
 @app.route('/api/protected-data', methods=['GET'])
 @token_required
